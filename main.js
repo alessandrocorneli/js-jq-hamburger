625c89b9fd97f05f0914bc279a9d9d0edd1edc53
Alessandro <alessandro.corneli11@mail.com>
2020-01-17 00:58:26 +0100

    header menu show/hide

---
+++ main.js
@@ -0,0 +1,15 @@
+// test click show menu:
+
+$('.header-right .fas.fa-bars').on('click', function() {
+  $('.hamburger-menu').show();
+});
+
+// funzionante
+
+// test click hide menu:
+
+$('.fas.fa-times').on('click', function() {
+  $('.hamburger-menu').hide();
+});
+
+// funzionante, completo
